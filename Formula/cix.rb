14381ee534f414295d73b8ee5efc979a7b262afb
steve <tiep.forwork@gmail.com>
2026-05-19 14:23:05 +0700

    fix: correct URL to v0.1.4

--- Formula/cix.rb
+++ Formula/cix.rb
@@ -6,10 +6,10 @@ class Cix < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/tiepnguyen-cix/cix/releases/download/v0.1.1/cix-darwin-arm64"
+      url "https://github.com/tiepnguyen-cix/cix/releases/download/v0.1.4/cix-darwin-arm64"
       sha256 "35222df7da1cc613841e373b785326d613bc2bef1bd753677d67e3b0ced51cee"
     else
-      url "https://github.com/tiepnguyen-cix/cix/releases/download/v0.1.1/cix-darwin-amd64"
+      url "https://github.com/tiepnguyen-cix/cix/releases/download/v0.1.4/cix-darwin-amd64"
       sha256 "9c46a013b47a64a9f4c9d2d7f3ff2dbfe0f1b14118f1a13bd1bb0658a4d56cb3"
     end
   end
