ff33d12dc821d41c3d41724cec4a552911907984
steve <tiep.forwork@gmail.com>
2026-05-19 13:49:05 +0700

    feat: update formula to v0.1.1

--- Formula/cix.rb
+++ Formula/cix.rb
@@ -1,16 +1,16 @@
 class Cix < Formula
   desc "Run and debug GitLab CI pipelines locally"
   homepage "https://github.com/tiepnguyen-cix/cix"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/tiepnguyen-cix/cix/releases/download/v0.1.0/cix-darwin-arm64"
-      sha256 "854f0a9eaab309ccd83a453b711e96acd77103cf7c757c0d781d20b730ccd9d5"
+      url "https://github.com/tiepnguyen-cix/cix/releases/download/v0.1.1/cix-darwin-arm64"
+      sha256 "e860556e574008c24611c8042bbc9d2ca04e95830fb2965fee501c6257d18dd2"
     else
-      url "https://github.com/tiepnguyen-cix/cix/releases/download/v0.1.0/cix-darwin-amd64"
-      sha256 "8a3ab253aee8936724dded6de838694d99004ea051e4c4334a80c5c5b9f7767a"
+      url "https://github.com/tiepnguyen-cix/cix/releases/download/v0.1.1/cix-darwin-amd64"
+      sha256 "fb0d27c7a2c427a30b36662bbcb13027ddc1dbd2d0ae2c445f4f7f6e74e86d2e"
     end
   end
 
